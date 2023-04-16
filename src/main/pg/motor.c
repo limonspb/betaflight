f690ef2f7938d3a624ac4059a7c46396367196a4
Ivan Efimov <gendalf44@yandex.ru>
2023-04-15 21:46:41 -0500

    RPM_LIMITER_PR

--- src/main/pg/motor.c
+++ src/main/pg/motor.c
@@ -61,6 +61,7 @@ void pgResetFn_motorConfig(motorConfig_t *motorConfig)
     motorConfig->maxthrottle = 2000;
     motorConfig->mincommand = 1000;
     motorConfig->digitalIdleOffsetValue = 550;
+    motorConfig->kv = 1960;
 
 #ifdef USE_DSHOT_DMAR
     motorConfig->dev.useBurstDshot = ENABLE_DSHOT_DMAR;
@@ -72,7 +73,7 @@ void pgResetFn_motorConfig(motorConfig_t *motorConfig)
     }
 #endif
 
-    motorConfig->motorPoleCount = 14;   // Most brushes motors that we use are 14 poles
+    motorConfig->motorPoleCount = 14;   // Most brushless motors that we use are 14 poles
 
     for (int i = 0; i < MAX_SUPPORTED_MOTORS; i++) {
         motorConfig->dev.motorOutputReordering[i] = i;
