04e74808b7cc6b93712ebfe06131361d5bcd1a81
Ivan Efimov <gendalf44@yandex.ru>
2023-10-19 17:42:49 -0500

    Keep Iterms at zero throttle for fixed wing mixer

--- src/main/fc/core.c
+++ src/main/fc/core.c
@@ -781,7 +781,8 @@ bool processRx(timeUs_t currentTimeUs)
     /* In airmode iterm should be prevented to grow when Low thottle and Roll + Pitch Centered.
      This is needed to prevent iterm winding on the ground, but keep full stabilisation on 0 throttle while in air */
     if (throttleStatus == THROTTLE_LOW && !airmodeIsActivated && !launchControlActive) {
-        pidSetItermReset(true);
+        const bool needResetIterm = !(isFixedWing() && ARMING_FLAG(ARMED)); // don't reset iterm for fixed wing when armed at zero throttle
+        pidSetItermReset(needResetIterm);
         if (currentPidProfile->pidAtMinThrottle)
             pidStabilisationState(PID_STABILISATION_ON);
         else
