9b2168f5e853eeb6f47b3a236e2553a59c196fae
limonspb <gendalf44@yandex.ru>
2024-04-23 23:54:06 -0500

    removed .tpa_cutoff_hz = 0 - merge fix

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -231,7 +231,6 @@ void resetPidProfile(pidProfile_t *pidProfile)
         .ez_landing_limit = 15,
         .ez_landing_speed = 50,
         .tpa_cutoff_hz = 100.0,
-        .tpa_cutoff_hz = 0,
         .iterm_raw_gyro = 0,
         .pterm_lpf_static_hz = {0, 0, 0},
         .pterm_lpf_type = {FILTER_PT1, FILTER_PT1, FILTER_PT1},
